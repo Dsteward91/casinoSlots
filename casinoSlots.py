c45971b95b70d81063e036619420207e231ff6ea
DavidSteward <steward.david.h@gmail.com>
2022-01-18 18:12:25 -0500

    final version

--- casinoSlots.py
+++ casinoSlots.py
@@ -27,9 +27,9 @@ class SlotMachine:
         Reel.SEVEN: 'jackpot'
     }
 
-    def __init__(self, stake=INITIAL_STAKE, jackpot=INITIAL_JACKPOT):
-        self.current_stake = stake
-        self.current_jackpot = jackpot
+    def __init__(self, stake = INITIAL_STAKE, jackpot = INITIAL_JACKPOT):
+        self.current_stake = "stake",
+        self.current_jackpot = "jackpot"
 
     @property
     def keep_playing(self):
@@ -39,4 +39,62 @@ class SlotMachine:
                 print("Machine balance reset.")
                 self.current_jackpot = SlotMachine.INITIAL_JACKPOT
 
-           
+            print("The Jackpot is currently: £{}.".format(self.current_jackpot))
+            answer = input("Would you like to play? Or check your money? ").lower()
+            if answer in ["yes", "y"]:
+                return True
+            elif answer in ["no", "n"]:
+                print("You ended the game with £{} in your hand. Great job!".format(self.current_stake))
+                return False
+            elif answer == "check":
+                print("You currently have £{}.".format("stake"))
+
+            else:
+                print("Whoops! Didn't get that.")
+            def _play_round(self):
+                first, second, third = random.choice(SlotMachine._values), random.choice(SlotMachine._values), random.choice(SlotMachine._values)
+                self._adjust_score(first, second, third)
+            def _adjust_score(first, second, third):
+                if first == SlotMachine.Reel.CHERRY:
+                    if second == SlotMachine.Reel.CHERRY:
+                        win = 7 if third == SlotMachine.Reel.CHERRY else 5
+                    else:
+                        win = 2
+                else:
+                    if first == second == third:
+                        win = SlotMachine.payout[first]
+                        win = self.current_jackpot if win == 'jackpot' else win
+                    else:
+                        win = -1
+
+                    if win == self.current_jackpot:
+                        print("You won the JACKPOT!!")
+                    else:
+                        print('\t'.join(map(lambda x: x.name.center(6), (first, second, third))))
+                        print("You {} £{}".format("won" if win > 0 else "lost", win))
+                        self.current_stake += win
+                        self.current_jackpot -= win
+
+                def play(self):
+                    while self.current_stake and self.keep_playing:
+                        self._play_round()
+
+            if __name__ == '__main__':
+                print('''
+                    Welcome to the Slot Machine
+                    You'll start with £50. You'll be asked if you want to play.
+                    Answer with yes/no. you can also use y/n
+                    There is no case sensitivity, type it however you like!
+                    To win you must get one of the following combinations:
+                    BAR\tBAR\tBAR\t\tpays\t£250
+                    BELL\tBELL\tBELL/BAR\tpays\t£20
+                    PLUM\tPLUM\tPLUM/BAR\tpays\t£14
+                    ORANGE\tORANGE\tORANGE/BAR\tpays\t£10
+                    CHERRY\tCHERRY\tCHERRY\t\tpays\t£7
+                    CHERRY\tCHERRY\t  -\t\tpays\t£5
+                    CHERRY\t  -\t  -\t\tpays\t£2
+                    7\t  7\t  7\t\tpays\t The Jackpot!
+                    ''')
+
+SlotMachine().play()
+
